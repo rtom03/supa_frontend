d6375e2bcbf03bbee76fcfdfbf745a6d8a1b9e9c
rtom03 <zuckm709@gmail.com>
2025-02-17 21:54:14 +0100

    page removed

---
+++ app/entertainments/page.js
@@ -0,0 +1,18 @@
+// import dynamic from "next/dynamic";
+
+// const Entertainments = dynamic(() => import("../path/to/Entertainment"), {
+//   ssr: false,
+// });
+
+// export default Entertainments;
+import React from 'react'
+
+const Entertainments = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Entertainments
